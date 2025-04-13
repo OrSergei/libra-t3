d625dcb039d518ca6b51411840a1658730525ccd
OrSergei <idipgpg@mail.ru>
2025-04-13 13:52:06 +0600

    добавление фильтрации по поиску

--- src/app/api/books/[id]/route.ts
+++ src/app/api/books/[id]/route.ts
@@ -1,4 +1,4 @@
-// src/app/api/books/[id]/route.ts
+
 import { NextResponse } from 'next/server';
 import { db } from '~/server/db';
 import { auth } from '~/server/auth';
@@ -20,6 +20,36 @@ export async function DELETE(
   } catch (error) {
     return NextResponse.json(
       { error: 'Failed to delete book' },
+      { status: 500 }
+    );
+  }
+}
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const session = await auth();
+  if (!session?.user || session.user.role !== 'LIBRARIAN') {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  try {
+    const body = await request.json();
+    
+    const updatedBook = await db.book.update({
+      where: { id: Number(params.id) },
+      data: {
+        title: body.title,
+        author: body.author,
+        year: body.year ? Number(body.year) : null,
+        description: body.description
+      }
+    });
+    
+    return NextResponse.json(updatedBook);
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to update book' },
       { status: 500 }
     );
   }
